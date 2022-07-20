033de0cbe61b3f8dcd4804aa9ad5d054cda52cae
popschool <contact@jibundeyare.com>
2022-07-20 16:33:11 +0200

    Améliorations

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,54 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Editor;
+use App\Entity\User;
+use App\Entity\Writer;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // Writer
+        $user = new User();
+        $user->setEmail('writer@example.com');
+        $user->setRoles(['ROLE_WRITER']);
+        $password = $this->encoder->encodePassword($user, '123');
+        $user->setPassword($password);
 
+        $manager->persist($user);
+        $manager->flush();
+
+        $writer = new Writer();
+        $writer->setUser($user);
+
+        $manager->persist($writer);
+        $manager->flush();
+
+        // Editor
+        $user = new User();
+        $user->setEmail('editor@example.com');
+        $user->setRoles(['ROLE_EDITOR']);
+        $password = $this->encoder->encodePassword($user, '123');
+        $user->setPassword($password);
+
+        $manager->persist($user);
+        $manager->flush();
+
+        $editor = new Editor();
+        $editor->setUser($user);
+
+        $manager->persist($editor);
         $manager->flush();
     }
 }
