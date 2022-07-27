f8ca7dad5aba4d8bd6ce7aa79801e3825faf38b0
popschool <contact@jibundeyare.com>
2022-07-27 14:58:07 +0200

    Améliorations

--- src/Controller/AdminArticleController.php
+++ src/Controller/AdminArticleController.php
@@ -11,6 +11,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\Exception\NotFoundException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 #[Route('/admin/article')]
 class AdminArticleController extends AbstractController
@@ -54,6 +56,11 @@ class AdminArticleController extends AbstractController
     #[Route('/new', name: 'app_admin_article_new', methods: ['GET', 'POST'])]
     public function new(Request $request, ArticleRepository $articleRepository): Response
     {
+        if (!$this->isGranted('ROLE_EDITOR') && !$this->isGranted('ROLE_WRITER')) {
+            // l'utilisateur n'est ni éditeur ni rédacteur, il ne peut pas créer de nouvel article
+            throw new AccessDeniedException();
+        }
+
         $article = new Article();
         $form = $this->createForm(ArticleType::class, $article);
         $form->handleRequest($request);
@@ -73,24 +80,7 @@ class AdminArticleController extends AbstractController
     #[Route('/{id}', name: 'app_admin_article_show', methods: ['GET'])]
     public function show(Article $article): Response
     {
-        // les utilisateurs non authentifiés sont renvoyés vers la page de login
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-
-        // méthode alternative à la méthode denyAccessUnlessGranted()
-        // if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
-        //     // l'utilisateur n'est pas authentifié
-        //     throw new AccessDeniedException();
-        // }
-
-        if (!$this->isGranted('ROLE_EDITOR') && $this->isGranted('ROLE_WRITER')) {
-            $user = $this->getUser();
-            $writer = $this->writerRepository->findByUser($user);
-            $articles = $writer->getArticles();
-            if (!$articles->contains($article)) {
-                // le rédacteur n'est pas auteur de l'article
-                throw new AccessDeniedException();
-            }
-        }
+        $this->filterUser($article);
 
         return $this->render('admin_article/show.html.twig', [
             'article' => $article,
@@ -100,6 +90,8 @@ class AdminArticleController extends AbstractController
     #[Route('/{id}/edit', name: 'app_admin_article_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Article $article, ArticleRepository $articleRepository): Response
     {
+        $this->filterUser($article);
+
         $form = $this->createForm(ArticleType::class, $article);
         $form->handleRequest($request);
 
@@ -118,10 +110,38 @@ class AdminArticleController extends AbstractController
     #[Route('/{id}', name: 'app_admin_article_delete', methods: ['POST'])]
     public function delete(Request $request, Article $article, ArticleRepository $articleRepository): Response
     {
+        $this->filterUser($article);
+
         if ($this->isCsrfTokenValid('delete'.$article->getId(), $request->request->get('_token'))) {
             $articleRepository->remove($article, true);
         }
 
         return $this->redirectToRoute('app_admin_article_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    private function filterUser(Article $article)
+    {
+        // les utilisateurs non authentifiés sont renvoyés vers la page de login
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        // méthode alternative à la méthode denyAccessUnlessGranted()
+        // if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
+        //     // l'utilisateur n'est pas authentifié
+        //     throw new AccessDeniedException();
+        //     // on peut aussi générer une erreur Not Found 404 
+        //     throw new NotFoundHttpException();
+        // }
+
+        if (!$this->isGranted('ROLE_EDITOR') && $this->isGranted('ROLE_WRITER')) {
+            $user = $this->getUser();
+            $writer = $this->writerRepository->findByUser($user);
+            $articles = $writer->getArticles();
+            if (!$articles->contains($article)) {
+                // le rédacteur n'est pas auteur de l'article
+                throw new AccessDeniedException();
+                // on peut aussi générer une erreur Not Found 404 
+                // throw new NotFoundHttpException();
+            }
+        }
     }
 }
